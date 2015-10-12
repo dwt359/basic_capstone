e888a207f2a02b4cfb3f4a0a6089bf7644191dcc
dwt359 <dwt359@mail.missouri.edu>
2015-10-12 18:47:39 -0500

    filter get variables

--- api/index.php
+++ api/index.php
@@ -6,6 +6,7 @@ $routes = array(
 
 $get = explode('/', $_GET['route']);
 $handlerRoute = array_shift($get);
+$get = array_filter($get, '!empty');
 $post = $_POST;
 
 if(!isset($routes[$handlerRoute])){
