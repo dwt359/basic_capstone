29d8c0bd01e93a99fbcef2f18f365e127b1b1c47
jacobmichaelS <muitprogram@mu-018150.dhcp.missouri.edu>
2015-10-14 20:28:02 -0500

    Ehhh
    Erm…didn't do much.  Mostly just moved some CSS around, started setting
    up the find ride page, and added a 'ride database' in the dashboard
    controller for testing purposes

--- app/components/dashboard/dashboardCtrl.js
+++ app/components/dashboard/dashboardCtrl.js
@@ -1,6 +1,13 @@
 theApp.controller('dashboardCtrl', ['$scope', '$state',  function($scope, $state){
 
-  $scope.seats = [{name: 'New Seat', description:'', price: 0.00}];
+  $scope.seats = [{name: 'Seat', description:'', price: 0.00}];
+
+  //This will be queried!
+  $scope.rides = [{driver: 'Jacob', seats: [{name: 'Passenger seat', description: 'Cool!', price: '10.00'},
+{name: 'Bitch seat', description: 'Have fun!', price: '5.00'}], description: 'Awesomest ride ever!', startCity: 'Columbia',
+startState: 'MO', endCity: 'Chicago', endState: 'IL'}, {driver: 'Dan', seats: [{name: 'Cool seat!', description: 'Cool!', price: '9.00'},
+{name: 'Middle seat', description: ':P', price: '6.00'}], description: 'Daddy Dans Sedan!', startCity: 'St Louis',
+startState: 'MO', endCity: 'Seattle', endState: 'WA'}];
 
   $scope.removeSeat = function(i){
     if($scope.seats.length > 1){
@@ -19,6 +26,10 @@ theApp.controller('dashboardCtrl', ['$scope', '$state',  function($scope, $state
     //More janky routing crap
     if($state.is('dashboard.begin')){
       $state.go('home');
+    }
+
+    if($state.is('dashboard.find')){
+        $state.go('^.^.home');
     }
 
     if($state.is('dashboard.give.info') || $state.is('dashboard.give.pricing')){
