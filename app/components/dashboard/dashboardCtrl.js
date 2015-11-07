04471e9a295f340a4a8ff30998348724f324b875
rlh5z6 <rlh5z6@mail.missouri.edu>
2015-11-07 13:18:12 -0600

    final version of the profile

--- app/components/dashboard/dashboardCtrl.js
+++ app/components/dashboard/dashboardCtrl.js
@@ -6,6 +6,8 @@ theApp.controller('dashboardCtrl',  ['$scope', '$timeout', '$state', 'LoginAuth'
   $scope.states = ('AL AZ AR CA CO CT DE FL GA ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
             'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
             'WY').split(' ').map(function (state) { return { abbrev: state }; });
+
+    
   $scope.starting = {city: "", state: ""};
   $scope.ending = {city: "", state: ""};
 
